d30fd99a95a8658a585b26546b263351d6447a2c
hayate112 <k.hayate0112@gmail.com>
2021-05-02 03:39:00 +0900

    first commit

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,25 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 1123212;
+		System.out.println(long1);
+		
+		double double1 = 12314.32;
+		System.out.println(double1);
+		
+		boolean boolean1 = true;
+		System.out.println(boolean1);
+		
+		Float float1 = 1.1313f;
+		System.out.println(float1);
+		
+		Integer int1 = 13214;
+		System.out.println(int1);
+		
+		Character chara1 = 'あ';
+		System.out.println(chara1);
+		
+		int[] array = { 0, 10, 20, 30, 40 };
+		System.out.println(array[1]);
+	}
+}
---
+++ JavaWcp/src/Variable04.java
@@ -0,0 +1,14 @@
+
+public class Variable04 {
+	public static void main(String[] args) {
+		String str1;
+		str1 = "久木崎　颯";
+		System.out.println(str1);
+
+		str1 = "笑顔";
+		System.out.println(str1);
+
+		final int LUCKY_NUMBER = 7;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
